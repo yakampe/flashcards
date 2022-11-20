7e3263df96d2968ccceff78c1fed19c8f6dca57f
Yanis Kampe <yanis.kampe.cv@gmail.com>
2022-11-20 18:37:59 +0000

    start up setup

--- start.sh
+++ start.sh
@@ -5,10 +5,13 @@ docker rmi flash-cards-java -f
 
 # # start the container stack
 # # (assumes the caller has permission to do this)
-docker-compose up -d
+docker compose up -d
 
 #Send json
 curl -i \
 -H "Accept: application/json" \
 -H "Content-Type:application/json" \
 -X POST --data @./flash-cards.json "http://localhost:8080/api/flashcards"
+
+open http://localhost:5000/
+start http://localhost:5000/
