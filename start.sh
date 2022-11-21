9219ad7555d87dc261d88eadcc1c509b98684cde
Yanis Kampe <yanis.kampe.cv@gmail.com>
2022-11-21 22:19:09 +0000

    startup: start script to wait till backend is ready

--- start.sh
+++ start.sh
@@ -7,9 +7,17 @@ docker rmi flash-cards-java -f
 # # (assumes the caller has permission to do this)
 docker compose up -d
 
-#Send json
-sleep 5
+#Wait ill the API is up and running
+echo "Waiting for back-end to be up"
+isUnhealthy=''
+while [ -z "$isUnhealthy"  ]
+do
+    isUnhealthy=$(curl -si http://localhost:8080/)
+    sleep 1
+done
+echo "Backend up and running"
 
+#Send json
 curl -i \
 -H "Accept: application/json" \
 -H "Content-Type:application/json" \
